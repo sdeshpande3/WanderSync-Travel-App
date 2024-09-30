a74fb3de66bfafc93846e54bc9310a8bc27013f2
AravArora05 <aarora@gatech.edu>
2024-09-30 11:26:43 -0400

    Adding the updated Home Screen to the Repository. The nav bar currently isn't working yet

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sprintproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FrameLayout frameLayout;
+        frameLayout = (FrameLayout) findViewById(R.id.nav_bar);
+
+        /**
+         *
+        Button nextActivity = findViewById(R.id.nextActivityButton);
+
+
+        nextActivity.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+         */
     }
 }
