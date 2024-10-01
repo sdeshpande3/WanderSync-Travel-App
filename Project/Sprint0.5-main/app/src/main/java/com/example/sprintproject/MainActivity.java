00e459855ea031a0b7cc2634d6aaee72de186009
AravArora05 <aarora@gatech.edu>
2024-10-01 16:00:22 -0400

    Adding all the updated files with the correct nav bar, changed the functionality of updates in the past version

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/MainActivity.java
@@ -1,21 +1,95 @@
 package com.example.sprintproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.example.sprintproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    /**
+     * Taken inspiration from thsi video: https://www.youtube.com/watch?v=jOFLmKMOcK0 -->
+     *
+     */
+    ActivityMainBinding binding;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FrameLayout frameLayout;
-        frameLayout = (FrameLayout) findViewById(R.id.nav_bar);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
+        /**
+         * Bottom navigation bar
+         */
+        ImageView homeImage = findViewById(R.id.home_image);
+        homeImage.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+                                             replaceFragment(new HomeFragment());
+                                         }
+                                     });
+        binding.bottomNavigationView.setOnItemSelectedListener(clicked -> {
+
+
+                /**
+                case R.id.notebook:
+                    replaceFragment(new HomeFragment());
+                    break;
+                 */
+                if ( clicked.getItemId() == R.id.home_image) {
+                    replaceFragment(new HomeFragment());
+                }
+                if (clicked.getItemId() == com.example.sprintproject.R.id.destination) {
+                    replaceFragment(new DestinationFragment());
+                }
+
+                if (clicked.getItemId() == com.example.sprintproject.R.id.restaurant) {
+                    replaceFragment(new RestaurantFragment());
+                }
+
+            if (clicked.getItemId() == com.example.sprintproject.R.id.hotel) {
+                replaceFragment(new HotelFragment());
+            }
+
+            if (clicked.getItemId() == R.id.community) {
+                replaceFragment(new CommunityFragment());
+            }
+            if (clicked.getItemId() == R.id.car) {
+                replaceFragment(new CarFragment());
+            }
+
+
+                    /**
+                    replaceFragment(new DestinationFragment());
+                    break;
+                case R.id.restaurant:
+                    replaceFragment(new RestaurantFragment());
+                    break;
+                case R.id.hotel:
+                    replaceFragment(new HotelFragment());
+                    break;
+                case R.id.community:
+                    replaceFragment(new CommunityFragment());
+                    break;
+                case R.id.car:
+                    replaceFragment(new CarFragment());
+                    break;
+                     */
+            return true;
+
+
+        });
+    }
         /**
          *
         Button nextActivity = findViewById(R.id.nextActivityButton);
@@ -29,5 +103,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
          */
+
+        private void replaceFragment(Fragment fragment) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        }
     }
-}
+
+
--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/NotebookFragment.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/NotebookFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NavBar#newInstance} factory method to
+ * Use the {@link NotebookFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NavBar extends Fragment {
+public class NotebookFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class NavBar extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NavBar() {
+    public NotebookFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class NavBar extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NavBar.
+     * @return A new instance of fragment NotebookFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NavBar newInstance(String param1, String param2) {
-        NavBar fragment = new NavBar();
+    public static NotebookFragment newInstance(String param1, String param2) {
+        NotebookFragment fragment = new NotebookFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class NavBar extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_bar, container, false);
+        return inflater.inflate(R.layout.fragment_notebook, container, false);
     }
 }
