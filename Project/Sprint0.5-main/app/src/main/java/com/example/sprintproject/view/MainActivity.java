26848a99dcf4a9c2ffed036f02e235c54a8655d4
tuckerritti <45155694+tuckerritti@users.noreply.github.com>
2024-10-05 12:28:22 -0400

    Move stuff to MVVM architecture

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -1,17 +1,15 @@
-package com.example.sprintproject;
+package com.example.sprintproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import com.example.sprintproject.R;
 import com.example.sprintproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
